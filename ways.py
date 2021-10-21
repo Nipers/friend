4a64a8c599ed7551c9dd883465844300f5ac411d
Nipers <xylu18@gmail.com>
2021-10-21 11:09:05 +0800

    DP

---
+++ ways.py
@@ -0,0 +1,21 @@
+'''
+CSC373 Fall 2021
+Problem Set 2, How Many Ways?
+University of Toronto Mississauga
+'''
+
+# Do NOT add any "import" statements
+
+def num_ways(t, p, n):
+    '''t, p, and n are as described in the handout.
+
+    Return the number of matches.
+    '''
+    pass  # TODO: implement this function
+
+
+if __name__ == '__main__':
+
+    # some small test cases
+    # Case 1
+    print(num_ways('aababc', 'aabc', 2))  # Answer should be 4
